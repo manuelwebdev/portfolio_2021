7a70a4f0da48f15ba6f5a40936b1daff7c1a6f76
manoy96 <mfewcar@gmail.com>
2022-01-12 21:35:33 -0700

    Brought in FontAwesome, working on card flip, About me

---
+++ main.js
@@ -0,0 +1,14 @@
+// flippable card on About Me section
+let box = document.querySelector(".statCard");
+box.addEventListener("click", function () {
+  box.classList.toggle("active");
+});
+
+function keyFunction(e) {
+  if (e.keyCode == 38) {
+    box.classList.add("active");
+  }
+  if (e.keyCode == 40) {
+    box.classList.remove("active");
+  }
+}
