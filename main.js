dfd643b35121c244a60a4ebbd3ec659e166d86a6
manuelwebdev <mfewcar@gmail.com>
2023-09-21 14:51:10 -0600

    updating resume file. made copyright year dynamic.

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-// flippable card on About Me section
-// let profile = document.querySelector(".statCard.profile");
-// let dev = document.querySelector(".statCard.dev");
-// let design = document.querySelector(".statCard.design");
-
-// let cards = [profile, dev, design];
-// cards.forEach((element) => {
-//   element.addEventListener("click", () => element.classList.toggle("active"));
-// });
+document.querySelector('.footTitle .year').innerHTML = new Date().getFullYear()
